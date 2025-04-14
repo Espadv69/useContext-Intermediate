09f11757068e2601befdaa6ff3110f97ef9b9662
Espadv69 <aitorjob69@gmail.com>
2025-04-14 19:45:44 +0200

    feat: implement Cart component logic for item management and total calculation

--- ShoppingCart-Context/src/components/Cart/Cart.jsx
+++ ShoppingCart-Context/src/components/Cart/Cart.jsx
@@ -1,6 +1,14 @@
 import { useCart } from '../../context/CartContext'
 import './Cart.css'
 
-const Cart = () => {}
+const Cart = () => {
+  const { cart, removeItem, clearCart } = useCart()
+
+  const getTotal = () => {
+    return cart
+      .reduce((acc, item) => acc + item.price * item.quantity, 0)
+      .toFixed(2)
+  }
+}
 
 export default Cart
