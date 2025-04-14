badb4a62159b08070a98cecc8216c2e4ed1d5b84
Espadv69 <aitorjob69@gmail.com>
2025-04-14 19:39:54 +0200

    feat: define ProductList component structure

--- ShoppingCart-Context/src/components/ProductList/ProductList.jsx
+++ ShoppingCart-Context/src/components/ProductList/ProductList.jsx
@@ -1,3 +1,7 @@
 import { products } from '../../data/productsData'
 import ProductItem from '../ProductItem/ProductItem'
 import './ProductList.css'
+
+const ProductList = () => {}
+
+export default ProductList
