dce03da9fde6c6dc8416e3fa36152d56b2b8692f
Espadv69 <aitorjob69@gmail.com>
2025-04-14 19:38:29 +0200

    feat: add Gaming Mouse product data to productsData.js

--- ShoppingCart-Context/src/data/productsData.js
+++ ShoppingCart-Context/src/data/productsData.js
@@ -11,4 +11,10 @@ export const products = [
     price: 149.99,
     image: 'https://via.placeholder.com/150',
   },
+  {
+    id: 3,
+    name: 'Gaming Mouse',
+    price: 49.99,
+    image: 'https://via.placeholder.com/150',
+  },
 ]
