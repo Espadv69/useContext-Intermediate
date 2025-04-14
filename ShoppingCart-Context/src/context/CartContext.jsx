c1c267111f221fe6bf9283e5541eccd7d3b72d85
Espadv69 <aitorjob69@gmail.com>
2025-04-14 19:23:12 +0200

    fix: add CartContext initialization and initialCart state setup

--- ShoppingCart-Context/src/context/CartContext.jsx
+++ ShoppingCart-Context/src/context/CartContext.jsx
@@ -1,3 +1,7 @@
 import { createContext, useReducer, useContext, useEffect } from 'react'
 import { cartReducer } from './cartReducer'
 import { ADD_ITEM, REMOVE_ITEM, CLEAR_CART } from './cartTypes'
+
+const CartContext = createContext()
+
+const initialCart = JSON.parse(localStorage.getItem('cart')) || []
