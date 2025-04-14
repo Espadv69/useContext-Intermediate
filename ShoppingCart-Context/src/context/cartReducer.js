ca0210e10891afccd924c11054b34df6dcb92c6d
Espadv69 <aitorjob69@gmail.com>
2025-04-14 19:13:15 +0200

    chore: implement cartReducer function to handle ADD_ITEM action

--- ShoppingCart-Context/src/context/cartReducer.js
+++ ShoppingCart-Context/src/context/cartReducer.js
@@ -1,1 +1,15 @@
 import { ADD_ITEM, REMOVE_ITEM, CLEAR_CART } from './cartTypes'
+
+export const cartReducer = (state, action) => {
+  switch (action.type) {
+    case ADD_ITEM:
+      const existingItem = state.find((item) => item.id === action.payload.id)
+      if (existingItem) {
+        return state.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        )
+      }
+  }
+}
