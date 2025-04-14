ca0cbce5c35d8bf81c787909060ffe46e97cc19b
Espadv69 <aitorjob69@gmail.com>
2025-04-14 19:41:35 +0200

    feat: implement ProductList component rendering

--- ShoppingCart-Context/src/components/ProductList/ProductList.jsx
+++ ShoppingCart-Context/src/components/ProductList/ProductList.jsx
@@ -2,6 +2,17 @@ import { products } from '../../data/productsData'
 import ProductItem from '../ProductItem/ProductItem'
 import './ProductList.css'
 
-const ProductList = () => {}
+const ProductList = () => {
+  return (
+    <section className="product-list">
+      <h2>Our Products</h2>
+      <div className="product-grid">
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+    </section>
+  )
+}
 
 export default ProductList
