28191ae52789b5c64fc805663ecaea1e05fcb8ac
Espadv69 <aitorjob69@gmail.com>
2025-04-16 20:01:48 +0200

    feat: initialize SettingsContext with default state for theme and language

--- UserSettings-Context/src/context/settings/SettingsContext.jsx
+++ UserSettings-Context/src/context/settings/SettingsContext.jsx
@@ -1,3 +1,10 @@
 import { createContext, useContext, useReducer } from 'react'
 import { settingsReducer } from './settingsReducer'
 import { TOGGLE_THEME, SET_LANGUAGE } from './settingsTypes'
+
+const SettingsContext = createContext()
+
+const initialState = {
+  theme: 'light',
+  language: 'en',
+}
