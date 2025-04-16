66c2494fa97c5085f5166e07b42978111f83a042
Espadv69 <aitorjob69@gmail.com>
2025-04-16 20:11:07 +0200

    feat: initialize AuthContext and set up initial state for authentication

--- UserSettings-Context/src/context/auth/AuthContext.jsx
+++ UserSettings-Context/src/context/auth/AuthContext.jsx
@@ -1,3 +1,10 @@
 import { createContext, useContext, useReducer } from 'react'
 import { authReducer } from './authReducer'
 import { LOGIN, LOGOUT } from './authTypes'
+
+const AuthContext = createContext()
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+}
