da9e7af3bfc714cd5b7a5320a5ad94ab5c3d8f3a
Espadv69 <aitorjob69@gmail.com>
2025-04-16 20:08:50 +0200

    feat: implement authReducer with LOGIN case for user authentication

--- UserSettings-Context/src/context/auth/authReducer.js
+++ UserSettings-Context/src/context/auth/authReducer.js
@@ -1,1 +1,12 @@
 import { LOGIN, LOGOUT } from './authTypes'
+
+export const authReducer = (state, action) => {
+  switch (action.type) {
+    case LOGIN:
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload,
+      }
+  }
+}
