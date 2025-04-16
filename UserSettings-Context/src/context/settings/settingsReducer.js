e74de2e7eeb05506af075376a257eeab6a167365
Espadv69 <aitorjob69@gmail.com>
2025-04-16 19:59:21 +0200

    feat: implement settingsReducer for managing theme toggling

--- UserSettings-Context/src/context/settings/settingsReducer.js
+++ UserSettings-Context/src/context/settings/settingsReducer.js
@@ -1,1 +1,11 @@
 import { TOGGLE_THEME, SET_LANGUAGE } from './settingsTypes'
+
+export const settingsReducer = (state, action) => {
+  switch (action.type) {
+    case TOGGLE_THEME:
+      return {
+        ...state,
+        theme: state.theme === 'light' ? 'dark' : 'light',
+      }
+  }
+}
