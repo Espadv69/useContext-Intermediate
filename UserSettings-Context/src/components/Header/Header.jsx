842ce8aea15bf18878731886e056d3de127e5aca
Espadv69 <aitorjob69@gmail.com>
2025-04-17 21:56:46 +0200

    feat: define Header component structure

--- UserSettings-Context/src/components/Header/Header.jsx
+++ UserSettings-Context/src/components/Header/Header.jsx
@@ -1,3 +1,7 @@
 import { useSettings } from '../../context/settings/SettingsContext'
 import { useAuth } from '../../context/auth/AuthContext'
 import './Header.css'
+
+const Header = () => {}
+
+export default Header
