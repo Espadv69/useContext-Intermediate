118db4843cc46533081985a20fd06a8f787381ea
Espadv69 <aitorjob69@gmail.com>
2025-04-17 21:57:54 +0200

    feat: add useAuth hook to Header component for authentication context

--- UserSettings-Context/src/components/Header/Header.jsx
+++ UserSettings-Context/src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import './Header.css'
 
 const Header = () => {
   const { theme, language, toggleTheme } = useSettings()
+  const { isAuthenticated, user, logout } = useAuth()
 }
 
 export default Header
