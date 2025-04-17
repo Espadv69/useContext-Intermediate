03f3bff297e53d23753cadc16162ff79d17f3b36
Espadv69 <aitorjob69@gmail.com>
2025-04-17 21:57:19 +0200

    feat: implement useSettings in Header component

--- UserSettings-Context/src/components/Header/Header.jsx
+++ UserSettings-Context/src/components/Header/Header.jsx
@@ -2,6 +2,8 @@ import { useSettings } from '../../context/settings/SettingsContext'
 import { useAuth } from '../../context/auth/AuthContext'
 import './Header.css'
 
-const Header = () => {}
+const Header = () => {
+  const { theme, language, toggleTheme } = useSettings()
+}
 
 export default Header
