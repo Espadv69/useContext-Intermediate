ee5b9c0013997a8a63571dd9f45e63b5609cf2a1
Espadv69 <aitorjob69@gmail.com>
2025-04-18 21:48:12 +0200

    feat: update App component to render SettingsPanel alongside Header

--- UserSettings-Context/src/App.jsx
+++ UserSettings-Context/src/App.jsx
@@ -1,7 +1,13 @@
 import Header from './components/Header/Header'
+import SettingsPanel from './components/SettingsPanel/SettingsPanel'
 
 const App = () => {
-  return <Header />
+  return (
+    <div className="app">
+      <Header />
+      <SettingsPanel />
+    </div>
+  )
 }
 
 export default App
