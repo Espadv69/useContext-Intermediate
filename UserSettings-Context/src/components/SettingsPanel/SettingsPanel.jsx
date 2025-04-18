1e62b581377b368c1dda9745d691c5ad59400b40
Espadv69 <aitorjob69@gmail.com>
2025-04-18 21:40:40 +0200

    feat: initialize SettingsPanel component with useSettings hook

--- UserSettings-Context/src/components/SettingsPanel/SettingsPanel.jsx
+++ UserSettings-Context/src/components/SettingsPanel/SettingsPanel.jsx
@@ -2,6 +2,8 @@ import { useSettings } from '../../context/settings/SettingsContext'
 import { useAuth } from '../../context/auth/AuthContext'
 import './SettingsPanel.css'
 
-const SettingsPanel = () => {}
+const SettingsPanel = () => {
+  const { language, setLanguage } = useSettings()
+}
 
 export default SettingsPanel
