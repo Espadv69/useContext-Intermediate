9d74fb7701e6685181b2a83a3703763c985ce0b6
Espadv69 <aitorjob69@gmail.com>
2025-04-18 21:38:39 +0200

    feat: implement SettingsPanel component structure

--- UserSettings-Context/src/components/SettingsPanel/SettingsPanel.jsx
+++ UserSettings-Context/src/components/SettingsPanel/SettingsPanel.jsx
@@ -1,3 +1,7 @@
 import { useSettings } from '../../context/settings/SettingsContext'
 import { useAuth } from '../../context/auth/AuthContext'
 import './SettingsPanel.css'
+
+const SettingsPanel = () => {}
+
+export default SettingsPanel
