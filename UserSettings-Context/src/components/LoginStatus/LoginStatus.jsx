033c5ada6a0736072fe57d6ce8055980c34903ca
Espadv69 <aitorjob69@gmail.com>
2025-04-18 21:56:35 +0200

    feat: implement LoginStatus component logic to retrieve authentication status

--- UserSettings-Context/src/components/LoginStatus/LoginStatus.jsx
+++ UserSettings-Context/src/components/LoginStatus/LoginStatus.jsx
@@ -1,6 +1,8 @@
 import { useAuth } from '../../context/auth/AuthContext'
 import './LoginStatus.css'
 
-const LoginStatus = () => {}
+const LoginStatus = () => {
+  const { isAuthenticated, user } = useAuth()
+}
 
 export default LoginStatus
