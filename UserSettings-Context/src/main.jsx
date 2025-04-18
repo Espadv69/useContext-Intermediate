b962974cc197ab4a23eefdabf93e9d338fd58734
Espadv69 <aitorjob69@gmail.com>
2025-04-18 21:51:29 +0200

    feat: import '@fontsource-variable/roboto' in main.jsx

--- UserSettings-Context/src/main.jsx
+++ UserSettings-Context/src/main.jsx
@@ -1,3 +1,5 @@
+import '@fontsource-variable/roboto'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { SettingsProvider } from './context/settings/SettingsContext'
