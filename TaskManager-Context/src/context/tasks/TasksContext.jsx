84fc8ec685bd1f9e01dc807223cc1326f8d80b7d
Espadv69 <aitorjob69@gmail.com>
2025-04-20 22:07:49 +0200

    feat: initialize TasksContext with createContext and initialTasks

--- TaskManager-Context/src/context/tasks/TasksContext.jsx
+++ TaskManager-Context/src/context/tasks/TasksContext.jsx
@@ -1,3 +1,7 @@
 import { createContext, useContext, useReducer } from 'react'
 import { tasksReducer } from './tasksReducer'
 import { ADD_TASK, REMOVE_TASK, TOGGLE_TASK } from './tasksTypes'
+
+const TasksContext = createContext()
+
+const initialTasks = []
