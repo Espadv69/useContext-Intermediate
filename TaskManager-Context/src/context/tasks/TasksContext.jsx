8eaa7dcc92a3473a4a3f11f1310402b7f7b11c6b
Espadv69 <aitorjob69@gmail.com>
2025-04-20 22:08:35 +0200

    feat: implement TasksProvider to manage task state with useReducer

--- TaskManager-Context/src/context/tasks/TasksContext.jsx
+++ TaskManager-Context/src/context/tasks/TasksContext.jsx
@@ -5,3 +5,7 @@ import { ADD_TASK, REMOVE_TASK, TOGGLE_TASK } from './tasksTypes'
 const TasksContext = createContext()
 
 const initialTasks = []
+
+export const TasksProvider = ({ children }) => {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks)
+}
