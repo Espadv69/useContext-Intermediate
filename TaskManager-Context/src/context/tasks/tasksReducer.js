e32cc1ba3698eef99a290285eddf8e18e67dc6e1
Espadv69 <aitorjob69@gmail.com>
2025-04-20 22:03:52 +0200

    feat: implement tasksReducer for task management

--- TaskManager-Context/src/context/tasks/tasksReducer.js
+++ TaskManager-Context/src/context/tasks/tasksReducer.js
@@ -1,1 +1,8 @@
 import { ADD_TASK, REMOVE_TASK, TOGGLE_TASK } from './tasksTypes'
+
+export const tasksReducer = (state, action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return [...state, action.payload]
+  }
+}
