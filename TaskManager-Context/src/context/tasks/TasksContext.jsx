2cb970a61b47dcc168ade2c1091df47afc344cd4
Espadv69 <aitorjob69@gmail.com>
2025-04-20 22:11:37 +0200

    feat: add useTasks hook for accessing task context in TasksProvider

--- TaskManager-Context/src/context/tasks/TasksContext.jsx
+++ TaskManager-Context/src/context/tasks/TasksContext.jsx
@@ -29,3 +29,11 @@ export const TasksProvider = ({ children }) => {
     </TasksContext.Provider>
   )
 }
+
+export const useTasks = () => {
+  const context = useContext(TasksContext)
+  if (!context) {
+    throw new Error('useTasks must be used within a TasksProvider')
+  }
+  return context
+}
